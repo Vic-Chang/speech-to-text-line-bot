fb85420d27e54f2f7e13ccd25e0d38be4e7f01b9
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2022-09-18 05:29:10 +0800

    Convert `m4a` file extension to `wav`

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from fastapi import Request, FastAPI, HTTPException
 from linebot import (AsyncLineBotApi, WebhookParser)
 from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, AudioMessage)
+from linebot.models import AudioMessage, TextSendMessage, TextMessage
+from pydub import AudioSegment
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,6 +17,10 @@ session = aiohttp.ClientSession()
 async_http_client = AiohttpAsyncHttpClient(session)
 line_bot_api = AsyncLineBotApi(channel_access_token, async_http_client)
 parser = WebhookParser(channel_secret)
+
+
+async def process_file():
+    pass
 
 
 @app.post("/callback")
@@ -33,18 +38,26 @@ async def handle_callback(request: Request):
     for event in events:
         if isinstance(event.message, AudioMessage):
             message_content = await line_bot_api.get_message_content(event.message.id)
-            with open('Audio.M4A', 'wb') as fd:
+            file_name = 'Audio.M4A'
+            with open(file_name, 'wb') as fd:
                 async for chunk in message_content.iter_content():
                     fd.write(chunk)
+
+            request_audio = AudioSegment.from_file(file_name, format="M4A")
+            file_handle = request_audio.export(file_name + '.wav', format="wav")
+            print(type(file_handle))
+            # audio_segment = AudioSegment.from_file(file_name, format="wav")
 
             # ## Return message
             # await line_bot_api.reply_message(
             #     event.reply_token,
             #     TextSendMessage(text=event.message.text)
             # )
+
     return 'OK'
 
 
 @app.get("/")
 async def alive():
+    print('test')
     return {"message": "Alive"}
